383e18f92dd5a189b0ca399aa5fbc3876899c1b7
Pradip Kumar Kushwaha <u2489889@uel.ac.uk>
2024-12-25 10:43:11 +0000

    week 5

--- index.js
+++ index.js
@@ -1,66 +1,31 @@
-var express=require("express")
-var fs= require("fs")
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import GreetingElement from './mygreetingapp';
+import GreetingElementwithProp from './mygreetingprops';
+import AppColor from './Appbgcolor';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+  <App/>
+    <GreetingElement/>
+    
+ <GreetingElementwithProp  msg="Hello guys.Today is begining of week i.e. Monday"/>
+ <GreetingElementwithProp  msg="Hello guys.Today is Tuesday"/>
+ <GreetingElementwithProp  msg="Hello guys.Today is Wednesday"/>
+ <GreetingElementwithProp  msg="Hello guys.Today is Thursday"/>
+ <GreetingElementwithProp  msg="Hello guys.Today is end of week i.e. Friday"/>
+ <GreetingElementwithProp  msg="Hello guys.Today is weekend i.e. Saturday"/>
+ <GreetingElementwithProp  msg="Hello guys.Today is weekend i.e. Sunday"/>
+ <AppColor heading="This is first element" lbl="Name :" color="green"/>
+ <AppColor heading="This is second element" lbl="Name :" color="blue"/>
+ <AppColor heading="This is third  element" lbl="Name :" color="Yellow"/>
+  </React.StrictMode>
+);
 
-var app=express()
-
-var bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.get('/',function(req,res){
-res.send("hello it is my first express application")
-});
-
-//exercise 2
-app.get('/about',function(req,res)
-{ res.send("This is my first basic express application ")});
-app.get('/users/:userId/books/:bookId', function (req, res) 
-{res.send(req.params)})
-
-//exercise 3
-app.get('/GetStudents',function (req,res){
-     studentdata={}
- fs.readFile(__dirname + "/" + "Student.json", 'utf8',
-function (err, data) { console.log( data );
- res.json({ 'status':true, 'Status_Code':200,'requested at': req.localtime, 'requrl':req.url,
- 'request Method':req.method, 'studentdata':JSON.parse(
-data)});
-});
-});
-
-app.get('/GetStudentid/:id', (req,res)=>{
-    studentdata={}
-    fs.readFile(__dirname + "/" + "Student.json", 'utf8', function (err, data) {
-   
-    var students= JSON.parse(data)
-    var student=students["Student"+req.params.id]
-    console.log("student",student)
-    if (student)
-   
-    res.json(student)
-    else
-    res.json({ 'status':true, 'Status_Code':200,
-    'requested at': req.localtime, 'requrl':req.url,
-    'request Method':req.method, 'studentdata':JSON.parse(data)});
-});
- });
-
- //exercise 4
- app.get('/studentinfo',function(req,res)
-{
-res.sendFile('StudentInfo.html', { root: __dirname });
-})
-app.post('/submit-data', function (req, res) {
-    var name = req.body.firstName + ' ' + req.body.lastName+
-    ' ';
-    var Age= req.body.myAge+ ' Gender: ' + req.body.gender+'' 
-    Qual= ' Qualification'+ req.body.Qual
-    console.log(req.body.Qual)
-    res.send({
-    status: true,
-    message: 'form Details', data: {
-    name: name, age:Age, Qualification:Qual,
-    }
-    });
-    });
-app.listen(4000,function(){console.log("server is running on port 4000")});
-
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
