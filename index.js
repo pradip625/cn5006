62cf8455188d2850c3b87c37c4bc63ecb31f50b4
Pradip Kumar Kushwaha <u2489889@uel.ac.uk>
2024-12-25 10:51:08 +0000

    week 3 codes

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-//console.log("this is my first visual code")
-//console.log("congrates your first code is successful")
+function employeeinfo(name, salary){
+    console.log( "employee name  "+name)
+    console.log(" employee slalary  "+salary)
+}
+console.log("this is my first js programme");
+var esalary = 4000
+var ename = "hari"
+employeeinfo( ename, esalary)
+// section 2 creating arrow function
+const Employeeskill= ( skills)=>{
+ console.log( "expert in  "+skills)
+}
+Employeeskill(" java ")//
 
-//example 2
-const num1=10;
-const num2= 4;
-const sum = num1+num2;
-console.log(" the sum of numbers = " +sum)
-const sub = num1-num2;
-console.log(" the sum of numbers = " +sub)
-const multply = num1*num2;
-console.log(" the sum of numbers = " +multply)
-const div = num1/num2;
-console.log(" the sum of numbers = " +div)
+ //section 3
+const student = require('./studentinfo')
+const person = require('./person')
+
+ console.log("student name is  ",student.getName())
+ console.log("student location is  ",student.location())
+ console.log(student.studentgrade(70))
+console.log( "date of birth is  ",student.dob)
+
+person1 = new person( " ram ", " USA", "ram@uel.ac.uk")
+console.log("using person module  ", person1.getpersoninfo() )
+//section 4
+const exercise = require('./exercise4')
+
+
